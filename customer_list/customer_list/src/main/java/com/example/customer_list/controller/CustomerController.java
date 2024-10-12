e56195a2d8e21117eb90350bbeb75da744910c3d
itachialy <zassuo@gmail.com>
2024-10-12 16:12:39 +0700

    update 2

--- customer_list/customer_list/src/main/java/com/example/customer_list/controller/CustomerController.java
+++ customer_list/customer_list/src/main/java/com/example/customer_list/controller/CustomerController.java
@@ -1,13 +1,14 @@
 package com.example.customer_list.controller;
 
 import com.example.customer_list.model.Customer;
-import com.example.customer_list.service.CustomerService;
 import com.example.customer_list.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -17,10 +18,22 @@ public class CustomerController {
     @GetMapping("")
 
 
-    public String showList(Model model){
+//    public String showList(Model model){
+//        List<Customer> list = iService.findAll();
+//        model.addAttribute("customers", list);
+//        System.out.println(list);
+//        return "list";
+//    }
+
+//    public ModelAndView showList(){
+//        ModelAndView modelAndView = new ModelAndView("list");
+//        List<Customer> list = iService.findAll();
+//        modelAndView.addObject("customers", list);
+//        return modelAndView;
+//    }
+    public String showList(HttpServletRequest request){
         List<Customer> list = iService.findAll();
-        model.addAttribute("customers", list);
-        System.out.println(list);
+        request.setAttribute("customers",list);
         return "list";
     }
 }
